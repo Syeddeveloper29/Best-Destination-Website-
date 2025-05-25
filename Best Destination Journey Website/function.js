6710d7ee8130ac42bee1ce37a65064f5a458f8e0
Syed Mehdi <158597106+Syeddeveloper29@users.noreply.github.com>
2025-05-26 04:03:58 +0500

    Added Updated Files On 26 May 2025

--- Best Destination Journey Website/function.js
+++ Best Destination Journey Website/function.js
@@ -1,8 +1,16 @@
-burger = document.querySelector('.burger')
-navbar = document.querySelector('.navbar')
-right = document.querySelector('.right')
+const navbar = document.querySelector('#navbar')
+const burger = document.querySelector('.burger')
+const icon = document.querySelector('.fa-bars');
 
 burger.addEventListener('click', () => {
-    right.classList.toggle('v-resp');
-    navbar.classList.toggle('h-resp');
-})
+    const isOpen = navbar.classList.contains('responsive');
+    if(!isOpen){
+        navbar.classList.add('responsive');
+        icon.classList.replace('fa-bars', 'fa-xmark');
+    }else{
+        navbar.classList.remove('responsive');
+        setTimeout( ()=>{
+            icon.classList.replace('fa-xmark', 'fa-bars');
+        }, 400);
+    }
+});
