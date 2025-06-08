22462db16f68042f3c54cbb7d4add2e32166fbd2
Syed Mehdi <158597106+Syeddeveloper29@users.noreply.github.com>
2025-06-09 00:43:10 +0500

    Delete function.js

--- function.js
+++
@@ -1,8 +0,0 @@
-burger = document.querySelector('.burger')
-navbar = document.querySelector('.navbar')
-right = document.querySelector('.right')
-
-burger.addEventListener('click', () => {
-    right.classList.toggle('v-resp');
-    navbar.classList.toggle('h-resp');
-})
